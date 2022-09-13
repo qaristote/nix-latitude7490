871131cc0fc95ad78355e486aed8761097d3e9a8
Quentin Aristote <quentin@aristote.fr>
2022-09-13 16:37:05 +0200

    nix: add tweag cache stores

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -32,7 +32,10 @@
       dates = "daily";
       options = "--delete-old";
     };
+    binaryCaches          = [ "https://hydra.iohk.io" "https://iohk.cachix.org" ];
+    binaryCachePublicKeys = [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo=" ];
   };
+
   system.autoUpgrade = {
     enable = true;
     flags = [ "--upgrade-all" ];
