4eb1cde9c34bc252689178e0b372ac269684c700
Quentin Aristote <quentin@aristote.fr>
2022-09-14 09:09:40 +0200

    firefox: debug arkenfox userjs

--- home/pkgs/firefoxPackages/default.nix
+++ home/pkgs/firefoxPackages/default.nix
@@ -9,6 +9,6 @@ in {
   arkenfox-userjs = pkgs.fetchurl {
     url =
       "https://github.com/arkenfox/user.js/blob/3f09afdee07c5aea33f6dc4194e1425d6400a0d7/user.js";
-    sha256 = "sha256-1ViPWMXo3G6xo/yGkKeLkmOayUX72aBENDUeCyWSlus=";
+    sha256 = "sha256-vr8OycafY/ZjdBS8YztH9IsXlgL7FUesSi82GjmmxYA=";
   };
 }
