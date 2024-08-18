cdfab65f35a0f5cc4164498d9e9632d2b699f1bf
quentin@aristote.fr <quentin@aristote.fr>
2024-08-18 17:02:37 +0200

    nixos: nix: auto update flake (--update-input deprecated)
    Flake lock file updates:
    
    • Updated input 'home-manager':
        'github:nix-community/home-manager/afc892db74d65042031a093adb6010c4c3378422?narHash=sha256-QfM/9BMRkCmgWzrPDK%2BKbgJOUlSJnfX4OvsUupEUZvA%3D' (2024-08-02)
      → 'github:nix-community/home-manager/2598861031b78aadb4da7269df7ca9ddfc3e1671?narHash=sha256-Fy%2BKEvDQ%2BHc8lJAV3t6leXhZJ2ncU5/esxkgt3b8DEY%3D' (2024-08-18)
    • Updated input 'home-manager/nixpkgs':
        'github:NixOS/nixpkgs/52ec9ac3b12395ad677e8b62106f0b98c1f8569d?narHash=sha256-veKR07psFoJjINLC8RK4DiLniGGMgF3QMlS4tb74S6k%3D' (2024-07-28)
      → 'github:NixOS/nixpkgs/5e0ca22929f3342b19569b21b2f3462f053e497b?narHash=sha256-M0xJ3FbDUc4fRZ84dPGx5VvgFsOzds77KiBMW/mMTnI%3D' (2024-08-09)
    • Updated input 'my-nixpkgs':
        'github:qaristote/my-nixpkgs/028c26db490fea082df3d34becbac24fb822c612?narHash=sha256-APoEUBU89svai3ezW1jGhc29JmAMc4hjpEVHN2RbbGw%3D' (2024-08-03)
      → 'github:qaristote/my-nixpkgs/aa1d46a976db0d5daa82786e6a489a325f753038?narHash=sha256-bAC8Gf1z1Ab5usR1O/XGe9IZiJFSOjTUwa0txBPqBmk%3D' (2024-08-17)
    • Updated input 'my-nixpkgs/flake-parts':
        'github:hercules-ci/flake-parts/e5d10a24b66c3ea8f150e47dfdb0416ab7c3390e?narHash=sha256-yzcRNDoyVP7%2BSCNX0wmuDju1NUCt8Dz9%2BlyUXEI0dbI%3D' (2024-05-02)
      → 'github:hercules-ci/flake-parts/8471fe90ad337a8074e957b69ca4d0089218391d?narHash=sha256-XOQkdLafnb/p9ij77byFQjDf5m5QYl9b2REiVClC%2Bx4%3D' (2024-08-01)
    • Updated input 'my-nixpkgs/flake-parts/nixpkgs-lib':
        'https://github.com/NixOS/nixpkgs/archive/50eb7ecf4cd0a5756d7275c8ba36790e5bd53e33.tar.gz?narHash=sha256-QBx10%2Bk6JWz6u7VsohfSw8g8hjdBZEf8CFzXH1/1Z94%3D' (2024-05-02)
      → 'https://github.com/NixOS/nixpkgs/archive/a5d394176e64ab29c852d03346c1fc9b0b7d33eb.tar.gz?narHash=sha256-uFf2QeW7eAHlYXuDktm9c25OxOyCoUOQmh5SZ9amE5Q%3D' (2024-08-01)
    • Updated input 'my-nixpkgs/nixpkgs':
        'github:NixOS/nixpkgs/94035b482d181af0a0f8f77823a790b256b7c3cc?narHash=sha256-kjQkA98lMcsom6Gbhw8SYzmwrSo%2B2nruiTcTZp5jK7o%3D' (2024-05-02)
      → 'github:NixOS/nixpkgs/cd7b95ee3725af7113bacbce91dd6549cee58ca5?narHash=sha256-OTDg91%2BZzs2SpU3csK4xVdSQFoG8cK1lNUwKmTqERyE%3D' (2024-08-17)
    • Updated input 'my-nixpkgs/nur':
        'github:nix-community/NUR/5f91bfaba4e2a2a2fdcce3b711b76b9aed14cbde?narHash=sha256-DPXEeLRIHazWyMYVXmhvJlfSAV0aF9nc9y6k4vthx8U%3D' (2024-05-03)
      → 'github:nix-community/NUR/8aa95b420ad3c1d292114267d88e870d9f31fcde?narHash=sha256-sLt2lMpNwjcW0HjU1hbL1SsDcjolJbR/lku4PVw/xu8%3D' (2024-08-17)
    • Updated input 'nixos-hardware':
        'github:NixOS/nixos-hardware/14c333162ba53c02853add87a0000cbd7aa230c2?narHash=sha256-2xLM4sc5QBfi0U/AANJAW21Bj4ZX479MHPMPkB%2BeKBU%3D' (2024-07-30)
      → 'github:NixOS/nixos-hardware/c54cf53e022b0b3c1d3b8207aa0f9b194c24f0cf?narHash=sha256-IiH8jG6PpR4h9TxSGMYh%2B2/gQiJW9MwehFvheSb5rPc%3D' (2024-08-10)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/81610abc161d4021b29199aa464d6a1a521e0cc9?narHash=sha256-TcXjLVNd3VeH1qKPH335Tc4RbFDbZQX%2Bd7rqnDUoRaY%3D' (2024-08-02)
      → 'github:NixOS/nixpkgs/a0d6390cb3e82062a35d0288979c45756e481f60?narHash=sha256-uljX21%2BD/DZgb9uEFFG2dkkQbPZN%2Big4Z6%2BUCLWFVAk%3D' (2024-08-17)
    • Updated input 'stylix':
        'github:danth/stylix/feb2973dfa8232c07efbd2b48f11a5cfa2276570?narHash=sha256-3XpT9GMw50NCGT1Gd2YAwEjrEcFtDqnuQ7sRUcuU/Pc%3D' (2024-07-29)
      → 'github:danth/stylix/6858d08ed012bc6491cc92c13142104e56badf31?narHash=sha256-PkJTr9DWBQcR5Ru1fJpG80dtw0MLSxAZlKnhHHFAGIA%3D' (2024-08-16)

--- nixos/nix.nix
+++ nixos/nix.nix
@@ -1,16 +1,17 @@
-{pkgs, ...}: {
+{...}: {
   personal.nix = {
     enable = true;
-    autoUpgrade = true;
+    autoUpgrade = {
+      enable = true;
+      autoUpdateInputs = [
+        "latitude-7490/home-manager"
+        "latitude-7490/my-nixpkgs"
+        "latitude-7490/nixos-hardware"
+        "latitude-7490/nixpkgs"
+        "latitude-7490/stylix"
+      ];
+    };
     flake = "git+file:///etc/nixos";
     gc.enable = true;
   };
-
-  system.autoUpgrade.flags = pkgs.personal.lib.updateInputFlags [
-    "latitude-7490/home-manager"
-    "latitude-7490/my-nixpkgs"
-    "latitude-7490/nixos-hardware"
-    "latitude-7490/nixpkgs"
-    "latitude-7490/stylix"
-  ];
 }
